397bc3ab6d725aaf51ae5ab90d4a88cc56ab0ab0
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2020-06-04 18:50:25 -0300

    Commit-Initial

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rocketseat</title>
+
+    <!-- START CSS LINKS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/fontawesome.css" rel="stylesheet">
+    <link href="css/brands.css" rel="stylesheet">
+    <link href="css/solid.css" rel="stylesheet">
+
+    <!-- END CSS LINKS -->
+
+</head>
+<body>
+
+
+<header class="menu">
+    <div class="container" style="vertical-align: center;">
+        <div class="row">
+            <div class="col-lg-3 col-sm-7 justify-content-start menu-logo">
+                <div class="row">
+                    <img src="images/logo-rocketseat.svg">
+                    <h3 class="title">Rocketseat</h3>
+                </div>
+            </div>
+            <div class="col-lg-9 col-sm-4 justify-content-end">
+                <nav class="navbar navbar-expand-lg justify-content-end ">
+                    <button class="navbar-toggler justify-content-end" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <div class="col-lg-9">
+                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 justify-content-center">
+                                <li class="nav-item active">
+                                    <a class="nav-link menu-active" href="#">Home</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Start</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Bootcamp</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Blog</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 justify-content-end">
+                            <a class="btn btn-primary btn-menu" href="#" role="button">Login</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7" style="align-self: center; margin-top: 115px">
+                <h2 class="title-section">As melhores tecnologias em <br>programação direto ao ponto <br>e do jeito certo</h2>
+                <br>
+                <h5 class="title-section" style="font-weight: normal">No meio de tantas informações e da quantidade<br>de ferramentas que surgem todos os dias, voçê<br>precisa de alguém que te leve na direção certa.</h5>
+                <div>
+                    <a class="btn btn-primary btn-home" href="#" role="button">Embarque neste foguete</a>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="lottie justify-content-center">
+                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                    <lottie-player src="https://assets10.lottiefiles.com/temp/lf20_L3FHDK.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px; margin-left: 100px; margin-top: 110px;"  loop  autoplay></lottie-player>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="frame2">
+    <div class="container">
+        <div class="row" style="position: absolute;margin-top: -130px; margin-left: 20px">
+            <a href="" class="item-img-frame2 card" style="color: #7159c1; border: none;">
+                <img style="width: 170px; height: 170px; margin: auto;"  src="images/starter.svg">
+                <h5 style="text-align: center;">START</h5>
+            </a>
+            <a href="" class="item-img-frame2 card" style="color: #7159c1; border: none;">
+                <img style="width: 170px; height: 170px; margin: auto;"  src="images/bootcamp.svg">
+                <h5 style="text-align: center;">BOOTCAMP</h5>
+            </a>
+            <a href="" class="item-img-frame2 card" style="color: #7159c1; border: none;">
+                <img style="width: 170px; height: 170px; margin: auto;"  src="images/cellphone-vector-b434c39f3641023e2d8623994ea4c789.svg">
+                <h5 style="text-align: center;">COMUNIDADE</h5>
+            </a>
+        </div>
+    </div>
+</section>
+
+<section class="frame3">
+    <div class="row">
+        <div class="col-lg-3" style="margin: auto">
+            <img src="images/nextlevelweek_18baaf82af.svg">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <i class="fa fa-calendar icone"></i>
+                    <div><h5 style="color: white; margin: auto; margin-top: 180px;">1 a 7<br>Junho</h5></div>
+                </div>
+                <div class="col-lg-2">
+
+                </div>
+                <div class="col-lg-2">
+                    <i class="fa fa-check-circle icone"></i>
+                    <div><h5 style="color: white; margin: auto; margin-top: 230px">100% gratuito<br>100% Online</h5></div>
+                </div>
+                <div class="col-lg-5">
+                    <img style="width: 600px; height: auto; margin: auto;" src="images/toptech-9c47e462321d8de220d64e23ca9a1d00.svg">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- START JAVASCRIPTS  -->
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script defer src="js/brands.js"></script>
+<script defer src="js/solid.js"></script>
+<script defer src="js/fontawesome.js"></script>
+
+<!-- END JAVASCRIPTS  -->
+</body>
+</html>
